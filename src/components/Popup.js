4a680b6f85ab679b60a59386374d06a307b4afd0
Kristina <cristina.peneva@gmail.com>
2021-10-01 15:54:15 +0200

    popup fixed color theme

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -12,19 +12,19 @@ const Popup = props => {
             <div className="box">                
                 <div className="social-icons">
                     <FacebookShareButton url="https://kristinapeneva.com/tic-tac-toe-game">
-                        {props.theme === "theme1" ? <FacebookIcon size="1.5rem" round={true} className="icon icon-fb" quote="Check this awesome Tic-Tac-Toe game! I just love it <3" /> : <FontAwesomeIcon icon={faFacebook} className="icon icon-fb" />}
+                        <FontAwesomeIcon icon={faFacebook} className="icon icon-fb" />
                     </ FacebookShareButton>
                     <FacebookMessengerShareButton url="https://kristinapeneva.com/tic-tac-toe-game">
-                        {props.theme === "theme1" ? <FacebookMessengerIcon size="1.5rem" round={true} className="icon icon-msg" quote="Check this awesome Tic-Tac-Toe game! I just love it <3" /> : <FontAwesomeIcon icon={faFacebookMessenger} className="icon icon-msg" />}
+                        <FontAwesomeIcon icon={faFacebookMessenger} className="icon icon-msg" />
                     </ FacebookMessengerShareButton>
                     <WhatsappShareButton url="https://kristinapeneva.com/tic-tac-toe-game">
-                    {props.theme === "theme1" ? <WhatsappIcon size="1.5rem" round={true} className="icon icon-wa" /> : <FontAwesomeIcon icon={faWhatsapp} className="icon icon-wa" />}
+                         <FontAwesomeIcon icon={faWhatsapp} className="icon icon-wa" />
                     </ WhatsappShareButton>
                     <EmailShareButton url="https://kristinapeneva.com/tic-tac-toe-game">
-                        {props.theme === "theme1" ? <EmailIcon size="1.5rem" round={true} className="icon icon-eml" subject="A New Tic-Tac-Toe Game! Check it!"/> : <FontAwesomeIcon icon={faEnvelope} className="icon icon-eml" />}
+                        <FontAwesomeIcon icon={faEnvelope} className="icon icon-eml" />
                     </ EmailShareButton>
                     <TwitterShareButton url="https://kristinapeneva.com/tic-tac-toe-game">
-                    {props.theme === "theme1" ? <TwitterIcon size="1.5rem" round={true} className="icon icon-tw" title="Tic-Tac-Toe Game" /> : <FontAwesomeIcon icon={faTwitter} className="icon icon-tw" />}
+                        <FontAwesomeIcon icon={faTwitter} className="icon icon-tw" />
                     </ TwitterShareButton>
                 </div>
             </div>
